d7f20fc79c355d43678718f99ec11cb767b41018
Ahmed <ahmedsherif602@gmail.com>
2024-09-18 11:13:47 +0300

    Server is made

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT | 3300;
+
+app.listen(port, (req,res)=> console.log("Server is listening on port", port));
+
+app.get("",(req,res)=>{
+    res.send("2");
+})
